24c83b3ba840da6b09a321b984a5d0f968a87c39
Emil <emil.kawalko@vp.pl>
2019-02-08 10:58:26 +0100

    Updates

--- precache-manifest.284283e7fd135eb32e178115aa34f8cc.js
+++ precache-manifest.284283e7fd135eb32e178115aa34f8cc.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/football-app/static/js/runtime~main.c06de619.js"
   },
   {
-    "revision": "4af04be1381732f51ca1",
-    "url": "/football-app/static/js/main.4af04be1.chunk.js"
+    "revision": "dfed2a51e5f949985384",
+    "url": "/football-app/static/js/main.dfed2a51.chunk.js"
   },
   {
     "revision": "2da35119d52b7c41cf55",
     "url": "/football-app/static/js/1.2da35119.chunk.js"
   },
   {
-    "revision": "4af04be1381732f51ca1",
+    "revision": "dfed2a51e5f949985384",
     "url": "/football-app/static/css/main.52af5ccf.chunk.css"
   },
   {
-    "revision": "d444e390ddabfbbdc3f333af2d88b1a5",
+    "revision": "a952e7fba95aacf1a77bcac2800eb27c",
     "url": "/football-app/index.html"
   }
 ];
