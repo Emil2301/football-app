6de3f8cc3a02d9255dccb4a2ea74825d4484df91
Emil <emil.kawalko@vp.pl>
2019-02-11 16:41:36 +0100

    update error displaying

--- src/components/Football.js
+++ src/components/Football.js
@@ -3,17 +3,23 @@ import './Football.css';
 
 //function component for displaying data
 const Football = props => {
+    if (props.error.stack === 'TypeError: Failed to fetch') {
+        props.error.stack = 'This API lets only 10 calls per minute. Try again after a moment.';
+    } else if(props.error) {
+    // if the error is other than too many API calls, then don't show a message 
+        props.error.stack = ''
+    }
     return (     
         <div>
             {
-                (props.name && !props.error) ? (
+                (props.name && !props.error.stack) ? (
                 <>
                     <div id="name">{props.name}</div>
                     <img src={`${props.image}`} alt=""/>
                 </>
             ) : <div>
                     <h3>
-                        {props.error ? String(props.error)+ ', because this API lets only 10 calls per minute ' : null}
+                        {props.error.stack ? props.error.stack : null}
                     </h3>
                 </div> 
             }             
--- src/components/NextMatch.js
+++ src/components/NextMatch.js
@@ -6,7 +6,7 @@ const NextMatch = props => {
     const date = new Date(props.date).toLocaleString();
     // checking if the error is caused with too many API calls
     if (props.error.stack === 'TypeError: Failed to fetch') {
-        props.error.stack = 'This API lets only 10 calls per minute. Try again after a moment';
+        props.error.stack = 'This API lets only 10 calls per minute. Try again after a moment.';
     } else if(props.error) {
     // if the error is other than too many API calls, then don't show a message 
         props.error.stack = ''
