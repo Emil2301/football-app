bc642ef24de082dd0bf16c417520daeaf877e90f
Kawalko <emil.kawalko@capgemini.com>
2019-05-31 12:45:15 +0200

    change name

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Routing.css'
 import DisplayLogo from './DisplayLogo'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Error from './Error'
 import Navigation from './Navigation'
 import WhenHePlays from './WhenHePlays'
 
-const Routing = () => {
-  return ( 
-    <Router basename={process.env.PUBLIC_URL}>
-      <>
-        <Navigation />
-        <Switch>
-          <Route exact path="/" component={DisplayLogo} />
-          <Route path="/when" component={WhenHePlays} />
-          <Route component={Error} />
-        </Switch>  
-      </>          
-    </Router>
-   );
+class Routing extends Component {
+  componentDidMount() {
+    document.title = "football-app"
+  }
+  render() {
+    return (
+      <Router basename={process.env.PUBLIC_URL}>
+        <>
+          <Navigation />
+          <Switch>
+            <Route exact path="/" component={DisplayLogo} />
+            <Route path="/when" component={WhenHePlays} />
+            <Route component={Error} />
+          </Switch>
+        </>
+      </Router>
+    );
+  }
+
 }
- 
+
 export default Routing;
 
