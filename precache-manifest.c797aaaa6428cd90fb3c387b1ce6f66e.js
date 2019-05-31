cd8d24ce424fbe46ceecb35edec2258936ac156e
Kawalko <emil.kawalko@capgemini.com>
2019-05-31 12:46:23 +0200

    Updates

---
+++ precache-manifest.c797aaaa6428cd90fb3c387b1ce6f66e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c06de6198ef57c73259d",
+    "url": "/football-app/static/js/runtime~main.c06de619.js"
+  },
+  {
+    "revision": "0c18705069688dfe127d",
+    "url": "/football-app/static/js/main.0c187050.chunk.js"
+  },
+  {
+    "revision": "89c4bcd685e7760a45e4",
+    "url": "/football-app/static/js/1.89c4bcd6.chunk.js"
+  },
+  {
+    "revision": "0c18705069688dfe127d",
+    "url": "/football-app/static/css/main.c267d73b.chunk.css"
+  },
+  {
+    "revision": "fe5f26ae1e114e17197df0c09c9e8e82",
+    "url": "/football-app/index.html"
+  }
+];
