d5c7caa360f54b302e903d11b669f1c7921cc14e
Kawalko <emil.kawalko@capgemini.com>
2019-05-31 11:20:45 +0200

    add spinner in whenheplays and displaylogo components

--- src/components/DisplayLogo.js
+++ src/components/DisplayLogo.js
@@ -2,33 +2,45 @@ import React, { Component } from 'react';
 import Form from './Form'
 import Football from './Football'
 import './DisplayLogo.css'
+import { css } from '@emotion/core';
+// First way to import
+import { ClipLoader } from 'react-spinners';
+
+const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+`;
 
 class DisplayLogo extends Component {
   state = {
-    error: '', 
-    name: '', 
-    image: ''
-  } 
+    error: '',
+    name: '',
+    image: '',
+    loading: false
+  }
   handleButtonClick = e => {
     //download the data of german Bundesliga teams
     const API = 'https://api.football-data.org/v2/competitions/2002/teams'
 
     fetch(API, {
-      headers:{
+      headers: {
         //my personal key to this API
         'X-Auth-Token': '8cf26ca4deb64429900d6be7f0576025'
       }
     })
       .then(res => {
-        if(res.ok) {
+        this.setState({ loading: true });
+        if (res.ok) {
           return res;
         }
         throw Error("Nie udało się: Błąd nr " + res.status)
       })
       .then(res => res.json())
       .then(data => {
+        this.setState({ loading: false });
         //Drawing a random number from range 0-17, because there are 18 teams in German Bundesliga and we start counting from 0
-        const random = Math.floor(Math.random()*18)
+        const random = Math.floor(Math.random() * 18)
         this.setState({
           error: '',
           name: data.teams[random].name,
@@ -44,13 +56,22 @@ class DisplayLogo extends Component {
   render() {
     return (
       <div className="App">
-        <Form 
-        click={this.handleButtonClick}     
+        <Form
+          click={this.handleButtonClick}
         />
-        <Football 
-        name={this.state.name}
-        image={this.state.image}
-        error={this.state.error}/>
+        <div className='sweet-loading'>
+          <ClipLoader
+            css={override}
+            sizeUnit={"px"}
+            size={100}
+            color={'#123abc'}
+            loading={this.state.loading}
+          />
+        </div>
+        <Football
+          name={this.state.name}
+          image={this.state.image}
+          error={this.state.error} />
       </div>
     );
   }
--- src/components/WhenHePlays.js
+++ src/components/WhenHePlays.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import './WhenHePlays.css'
 import NextMatch from './NextMatch'
+import { css } from '@emotion/core';
+// First way to import
+import { ClipLoader } from 'react-spinners';
+
+const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+`;
 
 class WhenHePlays extends Component {
-    state = { 
-        value: '', 
+    state = {
+        value: '',
         homeTeam: '',
         awayTeam: '',
         date: '',
-        error: ''
+        error: '',
+        loading: false
     }
     handleChange = e => {
         this.setState({
@@ -16,19 +26,22 @@ class WhenHePlays extends Component {
         });
         this.handleNextMatch(e.target.value)
     }
-    handleNextMatch = (playerNumber) => {          
+    handleNextMatch = (playerNumber) => {
         //downloading the array of future matches of the team
         const API = `https://api.football-data.org/v2/teams/${playerNumber}/matches?status=SCHEDULED`
 
         fetch(API, {
-            headers:{
+            headers: {
                 //my personal key to this API
                 'X-Auth-Token': '8cf26ca4deb64429900d6be7f0576025'
             }
         })
             .then(res => {
+                this.setState({
+                    loading: true
+                })
                 //if response.status is 200, then return response
-                if(res.ok) {
+                if (res.ok) {
                     return res;
                 }
                 throw Error("Nie udało się: Błąd nr " + res.status)
@@ -36,42 +49,57 @@ class WhenHePlays extends Component {
             // get json out of response
             .then(res => res.json())
             .then(data => {
-                const {homeTeam, awayTeam, utcDate} = data.matches[0];
+                this.setState({
+                    loading: false
+                })
+                const { homeTeam, awayTeam, utcDate } = data.matches[0];
                 this.setState({
                     homeTeam: homeTeam.name,
                     awayTeam: awayTeam.name,
                     date: utcDate
-                }) 
-            })            
+                })
+            })
             .catch(err => {
                 console.log(err)
                 this.setState({
                     error: err
-                })          
+                })
             })
     }
-    render() { 
-        const {value, homeTeam, awayTeam, date, error} = this.state
-        return ( 
-            <form>                           
-                <select 
-                onChange={this.handleChange}
-                value={this.state.value}
-                >	                
-                    <option value=''>Choose the player</option>
-                    <option value='5'>Robert Lewandowski</option>
-                    <option value='340'>Jan Bednarek</option>
-                    <option value='98'>Krzysztof Piątek</option>
-                    <option value='113'>Arkadiusz Milik</option>
-                    <option value='584'>Karol Linetty</option>                    
-                    <option value='4'>Łukasz Piszczek</option>                    
-                    <option value='341'>Mateusz Klich</option>                    
-                    <option value='322'>Kamil Grosicki</option>           
-                </select>
-                <NextMatch value ={value} homeTeam={homeTeam} awayTeam={awayTeam} date={date} error={error}/>  
-            </form>         
+    render() {
+        const { value, homeTeam, awayTeam, date, error } = this.state
+        return (
+            <>
+                <div className='sweet-loading'>
+                    <ClipLoader
+                        css={override}
+                        sizeUnit={"px"}
+                        size={150}
+                        color={'#123abc'}
+                        loading={this.state.loading}
+                    />
+                </div>
+                <form>
+                    <select
+                        onChange={this.handleChange}
+                        value={this.state.value}
+                    >
+                        <option value=''>Choose the player</option>
+                        <option value='5'>Robert Lewandowski</option>
+                        <option value='340'>Jan Bednarek</option>
+                        <option value='98'>Krzysztof Piątek</option>
+                        <option value='113'>Arkadiusz Milik</option>
+                        <option value='584'>Karol Linetty</option>
+                        <option value='4'>Łukasz Piszczek</option>
+                        <option value='341'>Mateusz Klich</option>
+                        <option value='322'>Kamil Grosicki</option>
+                    </select>
+                    <NextMatch value={value} homeTeam={homeTeam} awayTeam={awayTeam} date={date} error={error} />
+                </form>
+            </>
+
         );
     }
 }
- 
+
 export default WhenHePlays;
